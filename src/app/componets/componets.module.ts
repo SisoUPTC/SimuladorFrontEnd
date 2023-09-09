41e5c405d5893282fcbcd51d14cec1fba7d31f4b
BrayanCo <b.rayancol36@gmail.com>
2023-09-09 10:50:30 -0500

    Lateral Menu Implement :)

---
+++ src/app/componets/componets.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LateralMenuComponent } from './lateral-menu/lateral-menu.component';
+
+
+
+@NgModule({
+  declarations: [
+    LateralMenuComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    LateralMenuComponent
+  ]
+})
+export class ComponetsModule { }
